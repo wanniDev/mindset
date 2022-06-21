589a843bdd14a88bbdb51ed68ff2a3f4fae6bbf9
wanniDev <wannidev0928@gmail.com>
2022-06-21 23:53:55 +0900

    init project

---
+++ src/main/java/me/application/mindset/MindsetApplication.java
@@ -0,0 +1,13 @@
+package me.application.mindset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindsetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/application/mindset/MindsetApplicationTests.java
@@ -0,0 +1,13 @@
+package me.application.mindset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
